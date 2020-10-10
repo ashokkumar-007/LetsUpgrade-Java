de4ea1e85fce3d2b59661caff181405496365b8a
ashokkumar-007 <68260264+ashokkumar-007@users.noreply.github.com>
2020-10-10 23:51:16 +0530

    Add files via upload
    Assignment 1 Day 2

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.Employee;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+      Employee e = new Employee();
+      e.name="Sharmila";
+      e.age=18;
+      e.city="chennai";
+      e.display();
+      Employee e1 =  new Employee();
+      e1.name="Shiny";
+      e1.age=20;
+      e1.city="Helsinki";
+      e1.display();
+
+
+
+    }
+}
